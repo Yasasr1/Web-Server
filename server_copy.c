5fa9234b945dcd4d71bf97d0890658109bca5649
Yasasr1 <2017is070@stu.ucsc.cmb.ac.lk>
2020-01-18 21:23:00 +0530

    changes

--- server_copy.c
+++ server_copy.c
@@ -1,9 +1,8 @@
 #include "common.h"
 #include <strings.h>
 
-char *mime_type;
+char *outputType;
 
-void respond(int fd, char *header, void *body, int length);
 void handleFile(int fd, char *fileName);
 
 int main(int argc, char **argv)
@@ -11,23 +10,29 @@ int main(int argc, char **argv)
 	int listenfd, connfd, n;
 	struct sockaddr_in servaddr;
 
-	char buffer[1024] = {0}; 
-    	char requestType[4];       
-    	char requestpath[1024];
+	char browserInput[1024] = {0}; 
+    	char type[4];       
+    	char path[1024];
 
-	if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		printf("socket error");
+		return -1;
+		}
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(SERVER_PORT);
 
-	if((bind(listenfd, (SA *) &servaddr, sizeof(servaddr))) < 0)
+	if((bind(listenfd, (SA *) &servaddr, sizeof(servaddr))) < 0){
 		printf("bind error");
+		return -1;
+	}
 
-	if((listen(listenfd, 10)) < 0)
+	if((listen(listenfd, 10)) < 0){
 		printf("listen error");
+		return -1;
+		}
 
 	for( ; ; ) {
 		struct sockaddr_in addr;
@@ -38,9 +43,10 @@ int main(int argc, char **argv)
 
 		if((connfd = accept(listenfd, (SA *)&servaddr, (socklen_t*)&servaddr)) == -1){
 			printf("accept error");
+			return -1;
 		}
 
-		read( connfd, buffer, 1024);
+		read( connfd, browserInput, 1024);
 
 
 
@@ -48,50 +54,77 @@ int main(int argc, char **argv)
 		int j = 0;
 		bool isTypeSaved = false;
 
-		for(int i=0; i<=(strlen(buffer)); i++){
-			if(isTypeSaved == false && buffer[i] == ' '){
-				requestType[j] = '\0';
+		for(int i=0; i<=(strlen(browserInput)); i++){
+			if(isTypeSaved == false && browserInput[i] == ' '){
+				type[j] = '\0';
 				j = 0;
 				isTypeSaved = true;
 				continue;
 			}
-			else if(isTypeSaved == true && buffer[i] == ' '){
-				requestpath[j] = '\0';
+			else if(isTypeSaved == true && browserInput[i] == ' '){
+				path[j] = '\0';
 				j = 0;
 				break;
 			}
 			if(isTypeSaved == false){
-				requestType[j] = buffer[i];
+				type[j] = browserInput[i];
 				j++;
 			}else if(isTypeSaved == true){
-				requestpath[j] = buffer[i];
+				path[j] = browserInput[i];
 				j++;
 			}
 		}		
 		
-		printf("request Type: %s \n", buffer);
-		printf("%s %s \n",requestType,requestpath);
+		printf("request Type: %s \n", browserInput);
+		printf("%s %s \n",type,path);
 
-		for(int i=0; i< 100; i++){
-			requestpath[i] = tolower(requestpath[i]);
+
+		char extentionType[1024];
+		bool isSeperatorFound = false;
+		int k = 0;
+		for(int i=0; i<strlen(path); i++){
+			
+			if(path[i] == '.'){
+				isSeperatorFound = true;
+				continue;
+			}
+			
+			if(isSeperatorFound == true){
+				extentionType[k] = path[i];
+				k++;
+			}
+			
+			if(path[i] == '\0'){
+				extentionType[k] = '\0';
+				isSeperatorFound = false;
+				k = 0;
+	
+			}
 		}
 
-		char *mime = strrchr(requestpath, '.')+1;
-        	char *name = strtok(requestpath, "/");
+		if(strlen(path) <=2){
+			extentionType[0] = '\0';
+			
+		}
 
-		if(mime)
-			mime_type = mime;
+		char *name = strtok(path, "/");
+
+		printf("File type is:%s ",extentionType);
+
+
+		if(strlen(extentionType) > 1)
+			outputType = extentionType;
 		else
-			mime_type = NULL;
+			outputType = NULL;
 
-		if(!strcmp(requestType, "GET") && !strcmp(requestpath, "/")) {
+		if(!strcmp(type, "GET") && !strcmp(path, "/")) {
 			char *data = "Get request received: Displaying root page";
-			mime_type = html;
-			respond(connfd,"HTTP/1.1 200 OK", data, strlen(data));
-		}else if(!strcmp(requestType, "POST") && !strcmp(requestpath, "/")){
-			char *data = "Post request received";
-			mime_type = html;
-			respond(connfd,"HTTP/1.1 200 OK",data,strlen(data));
+			outputType = html;
+			char responce[strlen(data)+100];
+			int responce_length = sprintf(responce,"HTTP/1.1 200 OK\n""Connection: close\n""Content-length: %ld\n""Content-type: %s\n""\n",strlen(data),outputType);
+	
+			memcpy(responce + responce_length, data,strlen(data));
+			send(connfd, responce,responce_length + strlen(data), 0);
 		}else{
 			handleFile(connfd,name);
 		}
@@ -103,32 +136,37 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-void respond(int fd, char *HTMLheader, void *HTMLbody, int length){
-	char responce[length+100];
-	int responce_length = sprintf(responce,"%s\n""Connection: close\n""Content-length: %d\n""Content-type: %s\n""\n",HTMLheader,length,mime_type);
-	
-	memcpy(responce + responce_length, HTMLbody,length);
-	send(fd, responce,responce_length + length, 0);
-}
 
 void handleFile(int fd, char *name){
 	char *source;
 	FILE *file = fopen(name, "r");
-	size_t bufferSize;
+	size_t browserInputSize;
 
 	if(file != NULL){
 		if(fseek(file,0L,SEEK_END) == 0){
-			bufferSize = ftell(file);
-			source = malloc(sizeof(char) * (bufferSize + 1));
+			browserInputSize = ftell(file);
+			source = malloc(sizeof(char) * (browserInputSize + 1));
 			fseek(file, 0L, SEEK_SET);
-			fread(source, sizeof(char), bufferSize, file);
-			respond(fd, "HTTP/1.1 OK", source, bufferSize);
+			fread(source, sizeof(char), browserInputSize, file);
+
+			char responce[browserInputSize+100];
+			int responce_length = sprintf(responce,"HTTP/1.1 OK\n""Connection: close\n""Content-length: %ld\n""Content-type: %s\n""\n",browserInputSize,outputType);
+	
+			memcpy(responce + responce_length, source,browserInputSize);
+			send(fd, responce,responce_length + browserInputSize, 0);
 		}
 		free(source);
 		fclose(file);
 	}else{
 		char *error = "404: file cannot be found";
-        	mime_type = html;
-        	respond(fd, "HTTP/1.1 404 NOT FOUND", error, strlen(error));
+        	outputType = html;
+
+		char responce[strlen(error)+100];
+		int responce_length = sprintf(responce,"HTTP/1.1 404 NOT FOUND\n""Connection: close\n""Content-length: %ld\n""Content-type: %s\n""\n",strlen(error),outputType);
+	
+		memcpy(responce + responce_length, error,strlen(error));
+		send(fd, responce,responce_length + strlen(error), 0);
 	}
 }
+
+
